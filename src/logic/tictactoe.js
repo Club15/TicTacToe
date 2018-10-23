9211765e28296c1ef563680d22d2cb24e36b55b2
sjofno17 <sjofno17@ru.is>
2018-10-23 20:29:54 +0000

    profudum testing, ekki klarad

--- src/logic/tictactoe.js
+++ src/logic/tictactoe.js
@@ -1,6 +1,51 @@
+
 function tictactoe(title)
 {
-   return "this is " + title;
+  var mark = 'X';
+  var scoreO = 0;
+  var scoreX = 0;
+  var gridCounter = 0;
+  var haveWinner = false;
+  var table = ['1','2','3','4','5','6','7','8','9'];
+  var markedBoxes = [];
+
+  /*function sem flippar mark milli X og O
+  function sem hreinsar borðið / newgame
+  function sem hækkar grid coutnerinn
+  function sem tékkar hvort einhver sé búinn að vinna
+  function sem setur O og X í reit sem búið er að klikka á
+  function sem hækkar skorið hjá O og X
+  function sem tékkar hvort þa sé búið að klikka á reitinn*/
+
+
+  return "this is " + title;
 }
 
-module.exports = tictactoe;
+function flipMark(mark) {
+  if(mark === 'X') {
+    return 'O';
+  }
+  else {
+    return 'X';
+  }
+}
+
+function test(){
+  return "test";
+}
+
+function returnSomethingStupid(name){
+  return name;
+}
+
+module.exports = {};
+module.exports.tictactoe = tictactoe;
+module.exports.flipMark = function flipMark(mark) {
+  return "Testing flipMark";
+};
+module.exports.test = function tester(a, b) {
+  return a + b;
+};
+module.exports.returnSomethingStupid = function returnSomethingStupid() {
+  return "Hello";
+};
--- src/logic/tictactoe.test.js
+++ src/logic/tictactoe.test.js
@@ -1,4 +1,33 @@
-const tictactoe = require("./tictactoe");
-test("returns text", () => {
-   expect(tictactoe("Tic Tac Toe")).toBe("this is Tic Tac Toe");
-});
+var tictactoe = require("./tictactoe");
+
+//test("returns text", () => {
+   //expect(tictactoe.tictactoe("Tic Tac Toe") === "this is Tic Tac Toe")
+//});
+
+
+//const flipMark = require("./tictactoe");
+
+it("testing flipmark", () => {
+  //tictactoe.mark = 'X';
+  //tictactoe.flipMark();
+  expect(tictactoe.flipMark == "Testing flipMark");
+  //Þarf að setta mark fyrst sex x.
+  //Kalla á flipmark og staðfesta að það sé O
+
+	//Þarf að settta mark fyrst sem O
+	//Kalla á flipmark og staðfesta að það sé X
+})
+
+it("testing test function", () => {
+	expect(tictactoe.test(5, 6) == 10);
+})
+
+it("something stupid should reutrn somethingStupid", () => {
+	expect(tictactoe.returnSomethingStupid == "Hello");
+})
+
+//it("testing flipmark again", () => {
+  //tictactoe.mark = 'O';
+  //tictactoe.flipMark();
+  //expect(tictactoe.mark === 'X');
+//})
