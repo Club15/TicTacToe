12fb2b08e101539dec8f2b3b737d96385db63ed1
obelix09 <petra17.ru.is>
2018-10-23 23:10:48 +0000

    api test for GET /api/tictactoe/ fixed

--- src/logic/tictactoe.test.js
+++ src/logic/tictactoe.test.js
@@ -12,18 +12,3 @@ it("testing flipmark", () => {
 it("this should return X, since mark starts as X", () => {
   expect(getMark()).toBe('X');
 }); 
-
-//test("returns text", () => {
-   //expect(tictactoe.tictactoe("Tic Tac Toe") === "this is Tic Tac Toe")
-//});
-
-
-//const flipMark = require("./tictactoe");
-
-
-
-//it("testing flipmark again", () => {
-  //tictactoe.mark = 'O';
-  //tictactoe.flipMark();
-  //expect(tictactoe.mark === 'X');
-//})
