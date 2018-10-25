dffb8058c7bb56b1cecc3aee09f57d111ba7df1d
obelix09 <petra17.ru.is>
2018-10-25 17:21:57 +0000

    can click on each square

--- src/client/index.js
+++ src/client/index.js
@@ -1,28 +1,33 @@
 const _ = require("lodash");
 
-document.getElementById('getTable').addEventListener('click', getTable);
-
- function getTable(){
- 	console.log("button pressed");
-
+function getTable(){
     fetch('/api/tictactoe')
     .then(res =>
         res.json()
     )
     .then(function (body) {
-    	var table = document.getElementById('table');
-    	console.log(body);
     	var game = body.tictactoe.game;
-    	console.log(table);
-    	console.log(game);
     	for (var i = 0; i < game.length; i++)  {
-    		var square = document.createElement('div');
-    		square.id = i;
+    		var square = document.getElementById(i);
     		square.innerHTML = game[i];
-    		table.appendChild(square);
     	}
     })
- }
+}
 
- getTable();
+getTable();
+
+document.getElementById("0").onclick = function(){blob(0)};
+document.getElementById("1").onclick = function(){blob(1)};
+document.getElementById("2").onclick = function(){blob(2)};
+document.getElementById("3").onclick = function(){blob(3)};
+document.getElementById("4").onclick = function(){blob(4)};
+document.getElementById("5").onclick = function(){blob(5)};
+document.getElementById("6").onclick = function(){blob(6)};
+document.getElementById("7").onclick = function(){blob(7)};
+document.getElementById("8").onclick = function(){blob(8)};
+
+function blob(number) {
+	console.log(number);
+	alert(number);
+};
  
