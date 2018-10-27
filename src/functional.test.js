41679e6c340fe1a3a359ae0e2e207e1054f16f97
obelix09 <petra17.ru.is>
2018-10-27 18:48:45 +0000

    functional test checks when X has clicked on square, that O is next

--- src/functional.test.js
+++ src/functional.test.js
@@ -10,14 +10,8 @@ describe("TicTacToe", () => {
     });
 
     afterEach(() => {
-        browser.close();
+        browser.close(); 
     });
-
-    function delay(time) {
-        return new Promise(function(resolve) { 
-            setTimeout(resolve, time)
-        });
-     }
 
     // Check that the server responds with a 200 status message
     test("Server responds with 200 status code", async () => {
@@ -35,23 +29,59 @@ describe("TicTacToe", () => {
 
     //Check if X starts in the game
     test("When starting a game, it should be X turn", async () => {
-        const response = await page.goto(url);
-        //wait for half a sec to let the game load
-        await delay(500);
+        const response = await page.goto(url); 
+        //wait for game to load
+        await page.waitFor(500); 
+        //reset game
+        await page.click('#resetGame'); 
+        //wait for game to reload data
+        await page.waitFor(500);  
         const message = await page.evaluate(el => el.innerHTML, await page.$('h2'));
         expect(message).toBe("Its X turn!");
     });
 
 
     //Check if the scores are 0 at beginning of game
-    test("When starting a game, it should be X turn", async () => {
+    test("When starting a game, both scores should be 0", async () => {
         const response = await page.goto(url);
-        //wait for half a sec to let the game load
-        await delay(500);
+        //wait for game to load
+        await page.waitFor(500); 
+        //check the score for player X and player O
         const scoreO = await page.evaluate(el => el.innerHTML, await page.$('#scoreO'));
         const scoreX = await page.evaluate(el => el.innerHTML, await page.$('#scoreX'));
         expect(scoreO).toBe("Score O: 0");
         expect(scoreX).toBe("Score X: 0");
     });
 
+    //Check when player X clicks on a square, it gets marked by X
+        test("square should be marked X by player X", async () => {
+            const response = await page.goto(url);
+             //wait a bit for game to load 
+            await page.waitFor(500);
+            //click on square 0
+            await page.click('#s0');
+            //wait a bit for game to update
+            await page.waitFor(500);
+            //check if square 0 is now marked X
+            const square0 = await page.evaluate(el => el.innerHTML, await page.$('#s0'));
+            expect(square0).toBe("X");
+        });
+
+    //Check if player O is next when X has clicked a square
+        test("square should be marked X by player X", async () => {
+            const response = await page.goto(url);
+            //wait for game to load
+            await page.waitFor(500);
+            //click reset button
+            await page.click('#resetGame'); 
+            //wait for game to reload data
+            await page.waitFor(500);  
+            //click on square 0
+            await page.click('#s0');
+            //wait a bit for game to update
+            await page.waitFor(500);
+            //check if square 0 is now marked X
+            const message = await page.evaluate(el => el.innerHTML, await page.$('#msg'));
+            expect(message).toBe("Its O turn!");
+        });
 });
