5a0944df32fee7c193c0b97ca16bbf97f75953a8
obelix09 <petra17.ru.is>
2018-10-27 16:33:56 +0000

    functional test to check if scores starts at 0, and X starts the game

--- src/functional.test.js
+++ src/functional.test.js
@@ -13,6 +13,12 @@ describe("TicTacToe", () => {
         browser.close();
     });
 
+    function delay(time) {
+        return new Promise(function(resolve) { 
+            setTimeout(resolve, time)
+        });
+     }
+
     // Check that the server responds with a 200 status message
     test("Server responds with 200 status code", async () => {
         const response = await page.goto(url);
@@ -27,5 +33,25 @@ describe("TicTacToe", () => {
         // Check that the page title is "Google"
     });
 
+    //Check if X starts in the game
+    test("When starting a game, it should be X turn", async () => {
+        const response = await page.goto(url);
+        //wait for half a sec to let the game load
+        await delay(500);
+        const message = await page.evaluate(el => el.innerHTML, await page.$('h2'));
+        expect(message).toBe("Its X turn!");
+    });
+
+
+    //Check if the scores are 0 at beginning of game
+    test("When starting a game, it should be X turn", async () => {
+        const response = await page.goto(url);
+        //wait for half a sec to let the game load
+        await delay(500);
+        const scoreO = await page.evaluate(el => el.innerHTML, await page.$('#scoreO'));
+        const scoreX = await page.evaluate(el => el.innerHTML, await page.$('#scoreX'));
+        expect(scoreO).toBe("Score O: 0");
+        expect(scoreX).toBe("Score X: 0");
+    });
 
 });
